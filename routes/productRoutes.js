46d68b8e7abd95e402ca69968abf41d17bfcf691
Kayumovv <qayummovvv.7007@gmail.com>
2026-04-07 10:51:19 +0500

    chegirmaaaaaaaaaaaaaaaaaa

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import { getProducts, addProduct, deleteProduct, updateProduct } from '../controllers/productController.js';
+    import express from 'express';
+    import { getProducts, addProduct, deleteProduct, updateProduct } from '../controllers/productController.js';
 
-const router = express.Router();
+    const router = express.Router();
 
-router.get('/', getProducts);
-router.post('/', addProduct);
-router.put('/:id', updateProduct);  // ← qo'shing
-router.delete('/:id', deleteProduct);
+    router.get('/', getProducts);
+    router.post('/', addProduct);
+    router.put('/:id', updateProduct);  // ← qo'shing
+    router.delete('/:id', deleteProduct);
 
-export default router;
+    export default router;
